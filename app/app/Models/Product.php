93ab99ed1fefdc707995c32c3cd92e17f9edb98e
Andrey Khuzakhmetov <andruho978@gmail.com>
2025-06-19 13:33:39 +0700

    feature: add product table and model

---
+++ app/app/Models/Product.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @property int $category_id
+ * @property string $name
+ * @property string $description
+ * @property string $price
+ * ======================================
+ * Relations:
+ * @property Category $category
+ */
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+        'price',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
